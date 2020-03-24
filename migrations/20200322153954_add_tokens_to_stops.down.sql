9a43319835b28996f5932f741427dddc3a0557a2
Jiayu Yi <yijiayu@gmail.com>
2020-03-24 23:21:33 +0800

    Use trigger to update tsvector column

--- migrations/20200322153954_add_tokens_to_stops.down.sql
+++ migrations/20200322153954_add_tokens_to_stops.down.sql
@@ -1,2 +1,8 @@
+begin;
+
 alter table stops
     drop column tokens;
+drop trigger tsvectorupdate on stops;
+drop function stops_tokens_trigger;
+
+end;
--- migrations/20200322153954_add_tokens_to_stops.up.sql
+++ migrations/20200322153954_add_tokens_to_stops.up.sql
@@ -1,3 +1,24 @@
+begin;
+
 alter table stops
     add column tokens tsvector;
 create index stops_tokens_idx on stops using gin (tokens);
+
+CREATE FUNCTION stops_tokens_trigger() RETURNS trigger AS
+$$
+begin
+    new.tokens :=
+                    setweight(to_tsvector('english', new.id), 'A') ||
+                    setweight(to_tsvector('english', coalesce(new.description, '')), 'B') ||
+                    setweight(to_tsvector('english', coalesce(new.road, '')), 'C');
+    return new;
+end
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER tsvectorupdate
+    BEFORE INSERT OR UPDATE
+    ON stops
+    FOR EACH ROW
+EXECUTE FUNCTION stops_tokens_trigger();
+
+end;
