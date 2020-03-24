d3178bcfeb879989567c603e506c7dfb4a32f8be
Jiayu Yi <yijiayu@gmail.com>
2020-03-24 16:54:44 +0800

    Stop using generated column to support Postgres 11

--- migrations/20200322153954_add_tokens_to_stops.up.sql
+++ migrations/20200322153954_add_tokens_to_stops.up.sql
@@ -1,5 +1,3 @@
 alter table stops
-    add column tokens tsvector generated always as (setweight(to_tsvector('english', id), 'A') ||
-                                                    setweight(to_tsvector('english', coalesce(description, '')), 'B') ||
-                                                    setweight(to_tsvector('english', coalesce(road, '')), 'C')) stored;
+    add column tokens tsvector;
 create index stops_tokens_idx on stops using gin (tokens);
